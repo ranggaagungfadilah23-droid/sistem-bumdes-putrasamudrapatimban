1b816709158b65140bc593e3224cffacba9071da
xyuifdnsky <ranggaagungfadilah@gmail.com>
2026-05-25 14:34:40 +0700

    add Jasa model

---
+++ app/Models/Jasa.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jasa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'jasas';
+
+    protected $fillable = [
+        'user_id',
+        'nama_jasa',
+        'harga',
+        'satuan',
+        'deskripsi',
+        'gambar',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
